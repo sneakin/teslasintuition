a16bb38a6e08c18b3a501ebddff8cee305e33596
Nolan Eakins <sneakin@semanticgap.com>
2016-11-06 00:59:00 -0400

    Consolidated duplicated shaders, added color matrix.

--- code/quanta-1/shaders/basic_vertex.glsl
+++ code/quanta-1/shaders/basic_vertex.glsl
@@ -1,11 +1,16 @@
 #version 330
 layout(location = 0) in vec4 inPosition;
 layout(location = 1) in vec4 inColor;
-layout(location = 2) in vec2 inTexture;
+layout(location = 2) in vec4 inTexture;
 
-uniform mat4 ModelView, Projection;
+uniform mat4 mModelView, mProjection, mTexture, mColor;
+
+out vec4 Color;
+out vec2 Texture;
 
 void main()
 {
-  gl_Position = Projection * ModelView * inPosition;
+  Color = mColor * inColor;
+  Texture = vec2(mTexture * inTexture);
+  gl_Position = mProjection * mModelView * inPosition;
 }
