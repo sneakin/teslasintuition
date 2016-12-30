d39f14077e3466f3099143f65882fca25465b540
Nolan Eakins <sneakin@semanticgap.com>
2016-12-30 15:20:04 -0500

    Added an option to set the output movie's size.

--- code/scripts/shots2movie.sh
+++ code/scripts/shots2movie.sh
@@ -6,10 +6,11 @@ FPS_IN=${4:-30}
 FPS_OUT=${3:-30}
 
 # quanta-25316-2.bmp BMP3 800x600 800x600+0+0 8-bit sRGB 1.44MB 0.000u 0:00.000
+OSIZE=${5}
 SIZE=`identify "${PREFIX}"-1.bmp | cut -d " " -f 3`
 
-echo "Creating an $SIZE movie..."
+echo "Creating an ${OSIZE:-SIZE} movie..."
 
-avconv -r "${FPS_IN}" -f image2 -s "${SIZE}" -i "${PREFIX}"-%d.bmp -vcodec libx264 -crf 25 -r "${FPS_OUT}" "${OUTPUT}"
+avconv -r "${FPS_IN}" -f image2 -s "${SIZE}" -i "${PREFIX}"-%d.bmp -vcodec libx264 -crf 25 -r "${FPS_OUT}" -s "${OSIZE:-SIZE}" "${OUTPUT}"
 
 echo "All done. ${OUTPUT} was created."
